464c80b83a2870e22dd8d347904e62c52e65c0a9
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2024-05-05 16:44:18 -0400

    feat: add register screen

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { Text } from 'react-native';
 import { LoginScreen } from './src/Presentation/screens/login/LoginScreen.tsx';
+import { RegisterScreen } from './src/Presentation/screens/register/RegisterScreen.tsx';
 
 export default function App() {
   return (
     // <LoginScreen/>
-    <LoginScreen/>
+    <RegisterScreen/>
   );
 }
---
+++ src/Presentation/screens/register/RegisterScreen.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { View, Text, ScrollView, TextInput, TouchableOpacity, Image } from 'react-native';
+import { styles } from './Styles';
+
+export const RegisterScreen = () => {
+  return(
+    <View style={styles.container}>
+      <Image
+        style={styles.image} 
+        source={require('../../../../assets/background.png')}
+      />
+      <View style={styles.form}>
+        <Text style={styles.RegisterText}>Registrar Usuario</Text>
+        <View style={styles.inputSection}>
+          <View style={styles.inputContainer}>
+            <Image
+              style={styles.inputImage}
+              source={require('../../../../assets/user-icon.png')}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder={'Nombre'}
+              keyboardType='default'
+              secureTextEntry={false}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Image
+              style={styles.inputImage}
+              source={require('../../../../assets/user-icon.png')}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder={'Apellido'}
+              keyboardType='default'
+              secureTextEntry={false}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Image
+              style={styles.inputImage}
+              source={require('../../../../assets/phone-icon.png')}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder={'Teléfono'}
+              keyboardType='default'
+              secureTextEntry={false}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Image
+              style={styles.inputImage}
+              source={require('../../../../assets/email-icon.png')}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder={'Correo'}
+              keyboardType='default'
+              secureTextEntry={false}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Image
+              style={styles.inputImage}
+              source={require('../../../../assets/password-icon.png')}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder={'Contraseña'}
+              keyboardType='default'
+              secureTextEntry={false}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Image
+              style={styles.inputImage}
+              source={require('../../../../assets/password-icon.png')}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder={'Confirmar contraseña'}
+              keyboardType='default'
+              secureTextEntry={false}
+            />
+          </View>
+          <TouchableOpacity style={styles.registerButton}>
+            <Text style={styles.registerButtonText}>CONFIRMAR</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
+}
