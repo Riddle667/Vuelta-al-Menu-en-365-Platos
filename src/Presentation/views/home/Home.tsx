6315f2bddfde94f47d8144458302b3bd797be115
Shhapa <thomas.chapa@alumnos.ucn.cl>
2024-05-29 19:09:42 -0400

    feat(styles): add styles.tsx file to store view styles

--- src/Presentation/views/home/Home.tsx
+++ src/Presentation/views/home/Home.tsx
@@ -6,6 +6,7 @@ import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack'
 import { RootStackParamList } from '../../../../App';
 import useViewModel from './ViewModel';
+import styles from './Styles';
 import { CustomTextInput } from '../../components/CustomTextInput';
 
 export const HomeScreen = () => {
@@ -73,80 +74,7 @@ export const HomeScreen = () => {
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'black',
-    },
-    imageBackground: {
-        width: '100%',
-        height: '100%',
-        opacity: 0.7,
-        bottom: '30%'
-    },
-    form:{
-        width: '100%',
-        height: '40%',
-        backgroundColor: 'white',
-        position: 'absolute',
-        bottom: 0,
-        borderTopLeftRadius: 40,
-        borderTopRightRadius: 40,
-        padding: 30,
-    },
-    formText: {
-        fontWeight: 'bold',
-        fontSize: 25
-    },
-    formIcon: {
-        width: 30,
-        height: 30
-    },
-    formInput: {
-        flexDirection: 'row',
-        marginTop: 30
-    },
-    formTextInput: {
-        flex: 1,
-        backgroundColor: MyColors.primary,
-        borderWidth: 1,
-        borderColor: MyColors.primary,
-        borderRadius: 50, 
-        color: 'white', 
-        marginLeft: 10
-    },
-    formRegister: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 15, 
-    },
-    formRegisterText: {
-        fontStyle: 'italic',
-        color: MyColors.primary,
-        fontWeight: 'bold',
-        borderBottomWidth: 1,
-        borderBottomColor: MyColors.primary,
-        marginLeft: 10
-    },
-    logoContainer: {
-        position: 'absolute',
-        alignSelf: 'center', 
-        top: '15%'
-    },
-    logoImage: {
-        width: 120,
-        height: 120,
-        alignSelf: 'center'
-    },
-    logoText: {
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 20,
-        marginTop: 20,
-        fontWeight: 'bold'
-    },
 
-});
 
 
 
--- src/Presentation/views/register/Register.tsx
+++ src/Presentation/views/register/Register.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { Image, Text, View, TextInput, ToastAndroid, StyleSheet, TouchableOpacity,  } from 'react-native';
 import { RoundedButton } from '../../components/RoundedButton';
 import { MyColors } from '../../theme/AppTheme';
-import useViewModel from './ViewModel'
+import useViewModel from './ViewModel';
 import { CustomTextInput } from '../../components/CustomTextInput';
+import styles from './Styles';
 
 export const RegisterScreen = () => {
 
@@ -92,79 +93,6 @@ export const RegisterScreen = () => {
     );
 }
         
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'black',
-    },
-    imageBackground: {
-        width: '100%',
-        height: '100%',
-        opacity: 0.7,
-        bottom: '30%'
-    },
-    form:{
-        width: '100%',
-        height: '70%',
-        backgroundColor: 'white',
-        position: 'absolute',
-        bottom: 0,
-        borderTopLeftRadius: 40,
-        borderTopRightRadius: 40,
-        padding: 30,
-    },
-    formText: {
-        fontWeight: 'bold',
-        fontSize: 25
-    },
-    formIcon: {
-        width: 30,
-        height: 30
-    },
-    formInput: {
-        flexDirection: 'row',
-        marginTop: 30
-    },
-    formTextInput: {
-        flex: 1,
-        backgroundColor: MyColors.primary,
-        borderWidth: 1,
-        borderColor: MyColors.primary,
-        borderRadius: 50, 
-        color: 'white', 
-        marginLeft: 10
-    },
-    formRegister: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 15, 
-    },
-    formRegisterText: {
-        fontStyle: 'italic',
-        color: MyColors.primary,
-        fontWeight: 'bold',
-        borderBottomWidth: 1,
-        borderBottomColor: MyColors.primary,
-        marginLeft: 10
-    },
-    logoContainer: {
-        position: 'absolute',
-        alignSelf: 'center', 
-        top: '4%'
-    },
-    logoImage: {
-        width: 120,
-        height: 120,
-        alignSelf: 'center'
-    },
-    logoText: {
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 20,
-        marginTop: 20,
-        fontWeight: 'bold'
-    },
 
-});
 
 
