b38eb54883788a529a4b1107404fab80abddbac1
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2024-06-09 06:37:52 -0400

    fix(stack): clean stack's title

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { LoginScreen } from './src/Presentation/screens/login/LoginScreen.tsx';
 import { RegisterScreen } from './src/Presentation/screens/register/RegisterScreen.tsx';
 import 'react-native-gesture-handler';
@@ -9,14 +9,29 @@ import { AdminHomeScreen } from './src/Presentation/screens/admin/home/AdminHome
 import { ChangePasswordScreen } from './src/Presentation/screens/common/change_password/ChangePasswordScreen.tsx';
 import { CreateCategoryScreen } from './src/Presentation/screens/admin/category/create/CreateCategoryScreen.tsx';
 import FlashMessage from "react-native-flash-message";
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
+
+const CustomBackButton = ({navigation}) => {  
+  return (
+    <TouchableOpacity onPress={() => navigation.goBack()} style={{marginLeft: 20}}>
+      <Ionicons name="arrow-back" size={24} color="black"></Ionicons>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     // <LoginScreen/>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={LoginScreen}>
+      <Stack.Navigator
+        initialRouteName={LoginScreen}
+        screenOptions={({ navigation, route }) => ({
+          headerTitle: "",
+          headerLeft: route.name != "LoginScreen" ? () => <CustomBackButton navigation={navigation}/> : "",
+        })}
+      >
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />      
         <Stack.Screen name="AdminHomeScreen" component={AdminHomeScreen} />      
