9aa267516b4981310604b68de9ab62c48dcb0883
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2024-06-09 08:26:14 -0400

    removing files

--- src/Domain/useCases/userLocal/SaveUserLocal.tsx
+++
@@ -1,8 +0,0 @@
-import { UserLocalRepositoryImpl } from "../../../Data/repositories/UserLocalRepository";
-import { User } from "../../entities/User";
-
-const { save } = new UserLocalRepositoryImpl();
-
-export const SaveUserLocalUseCase = async (user: User) => {
-    return await save(user);
-}
